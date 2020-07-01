20b6ac0c01693a01ea840e7e56e3b72e0b6a0489
Nataliyi <ediga3@gmail.com>
2020-07-01 13:24:34 +0200

    New I am

--- Code/data2.py
+++ Code/data2.py
@@ -13,10 +13,30 @@ users_scores = sub_data.pivot_table(index='user_id',
                                     values='step_id',
                                     aggfunc='count',
                                     fill_value=0).reset_index()
+users_events_data = events_data.pivot_table(index='user_id',
+                                            columns='action',
+                                            values='step_id',
+                                            aggfunc='count',
+                                            fill_value=0).reset_index()
 gap_data = events_data[['user_id', 'day', 'timestamp']].drop_duplicates(subset=['user_id', 'day']) \
       .groupby('user_id')['timestamp'].apply(list).apply(np.diff).values
 gap_data = pd.Series(np.concatenate(gap_data, axis=0))
 gap_data = gap_data / (24 * 60 * 60)  # интересный перевод в дни
 print(gap_data[gap_data < 290].hist())  # собственно сам график
 print(gap_data.quantile(0.90))
-
+# events_data.sort_values(['correct'], ascending=False).groupby('user_id').head(5)  # вычисляем Анатолия))
+events_data.groupby('user_id')['day'].nunique().idxmax()  # Пользователь, который провел на курсе больше всего дней
+users_data = events_data.groupby('user_id', as_index=False) \
+    .agg({'timestamp': 'max'}).rename(columns={'timestamp': 'last_timestamp'})
+now = 1526772811
+drop_out_treshold = 259200  # тридцать дней (30 * 24 * 60 * 60)
+users_data['is_gone_user'] = (now - users_data.last_timestamp) > drop_out_treshold
+# финальный тайимстемп с *хвоста датафрейма минус последний таймстеп юзера
+users_data = users_data.merge(users_scores, on='user_id', how='outer').fillna(0)
+# объединяем таблицы, атрибут how говорит, что не хотим терять строки пустые, заполняем их nan
+users_data = users_data.merge(users_events_data, how='outer')
+users_days = events_data.groupby('user_id').day.nunique().to_frame().reset_index()
+# coздаем пандовскую серию, сколько дней у юзера, после переводим в датафрейм
+users_data = users_data.merge(users_days, how='outer')
+users_data['passed_course'] = users_data.passed > 170
+print(users_data.groupby('passed_course').count())
---
+++ Code/tree1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from sklearn import tree
+
+
+data = pd.DataFrame({'X_1': [1, 1, 1, 0, 0, 0, 0, 1], 'X_2': [0, 0, 0, 1, 0, 0, 0, 1], 'Y': [1, 1, 1, 1, 0, 0, 0, 0]})
+clf = tree.DecisionTreeClassifier(criterion='entropy')
+X = data[['X_1', 'X_2']]
+y = data.Y
+tree.plot_tree(clf.fit(X, y), feature_names=list(X), class_names=['Negative', 'Positive'], filled=True)
+# выводим дерево на экран, красота, только не понятно ничего пока))))
